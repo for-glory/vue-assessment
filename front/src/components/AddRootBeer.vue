438bfc01ddd89179c9213c2e23290af71dfda091
Justin Di Biase <jdemers2022@outlook.com>
2024-09-22 14:08:38 -0400

    implement add root beer page

---
+++ front/src/components/AddRootBeer.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Add a Root Beer</h1>
+    <form @submit.prevent="submitRootBeer" class="flex-column align-items-start">
+      <input v-model="newRootBeer.name" placeholder="Name" />
+      <textarea v-model="newRootBeer.description" placeholder="Description"></textarea>
+      <button type="submit">Add Root Beer</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+import { useRootBeerStore } from '@/stores/rootBeerStore';
+
+export default {
+  setup() {
+    const newRootBeer = ref({ name: '', description: '' });
+    const rootBeerStore = useRootBeerStore();
+
+    const submitRootBeer = async () => {
+      await rootBeerStore.addNewRootBeer(newRootBeer.value);
+      newRootBeer.value = { name: '', description: '' };
+    };
+
+    return { newRootBeer, submitRootBeer };
+  },
+};
+</script>
