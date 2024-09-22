65c9b48299e93449196060d6009215f58b35f48f
Justin Di Biase <jdemers2022@outlook.com>
2024-09-22 14:02:20 -0400

    configure custom proxy rules for the dev server

--- front/vite.config.js
+++ front/vite.config.js
@@ -14,5 +14,11 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  server: {
+    proxy: {
+      '/api': 'http://localhost:4000',
+      '/files': 'http://localhost:4000',
+    }
+  },
 })
